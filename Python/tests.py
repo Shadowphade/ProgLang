24493559a5fdcd4cb264d786f895394448f1ee64
rhysm <rhys.link.0919@gmail.com>
2024-04-17 20:31:37 -0500

    Added python tests

--- Python/tests.py
+++ Python/tests.py
@@ -1,7 +1,33 @@
 import unittest
 import mergeSort as MS
+import random
 
 class TestMergeSort(unittest.TestCase):
 
     def test_sort(self):
-        self.assertEqual()
+        testArr = [
+            {"pokemon":"Test","price":10.00},
+            {"pokemon":"Test","price":100.00},
+            {"pokemon":"Test","price":1000.00},
+            {"pokemon":"Test","price":1000.00},
+            {"pokemon":"Test","price":10000.00},
+            {"pokemon":"Test","price":10000.00},
+            {"pokemon":"Test","price":10001.00},
+            {"pokemon":"Test","price":100011.00},
+            {"pokemon":"Test","price":100011.00},
+            {"pokemon":"Test","price":1000111.00}
+        ]
+        pokeMonArr = []
+        for item in testArr:
+            pokeMonArr.append(MS.Pokemon(item["pokemon"], item["price"]))
+        testShuffled = pokeMonArr.copy()
+
+        random.shuffle(testShuffled)
+        MS.mergeSort(testShuffled, 0, len(testShuffled) - 1)
+
+        for i in range(len(pokeMonArr)):
+            self.assertEqual(pokeMonArr[i].price, testShuffled[i].price)
+
+
+if __name__ == '__main__':
+    unittest.main()
